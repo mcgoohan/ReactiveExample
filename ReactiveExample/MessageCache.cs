ef066236d5c235b8dfd20ee140171a50d92ef5dd
Damian McGoohan <damian.mcgoohan@sita.aero>
2022-04-11 10:41:32 +0100

    first commit

---
+++ ReactiveExample/MessageCache.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReactiveExample
+{
+    public class Message
+    {
+        public Message(string id)
+        {
+            MessageId = id;
+        }
+        
+        public string MessageId { get; set; }
+    }
+    
+    
+    class MessageCache
+    {
+        public IObservable<Message> AddItem(string messageId)
+        {
+            var subject = new Subject<Message>();
+            Task.Run(() => DoStuff(subject));
+            return subject;
+        }
+
+        private void DoStuff(Subject<Message> subject)
+        {
+            try
+            {
+                foreach (var n in Enumerable.Range(1, 10))
+                {
+                    Thread.Sleep(1000);
+                    subject.OnNext(new Message(n.ToString()));
+                }
+                subject.OnCompleted();
+            }
+            catch (Exception exception)
+            {
+                subject.OnError(exception);
+            }
+            finally
+            {
+                subject.Dispose();
+            }
+        }
+    }
+}
---
+++ ReactiveExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ReactiveExample
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var messageCache = new MessageCache();
+            
+            var observable = messageCache.AddItem("123");
+
+            observable.Subscribe(
+                message => Console.WriteLine("Message Expired:  {0}", message.MessageId));
+        }
+    }
+}
